93862ae7b74180d11d79c1e30ce688bc6eb915c6
byi649 <byi649@aucklanduni.ac.nz>
2022-09-12 05:06:19 +1000

    Trivial parallelism

--- OpenMP1/main.cpp
+++ OpenMP1/main.cpp
@@ -8,6 +8,8 @@
 #include <string>
 #include <queue>
 #include <omp.h>
+#include <cstring>
+#include <climits>
 
 using namespace std;
 #define V 100 // Max nodes in graph
@@ -17,16 +19,20 @@ using namespace std;
  * residual graph. Also fills parent[] to store the path */
 bool bfs(int rGraph[V][V], int s, int t, int parent[])
 {
+
 	// Create a visited array and mark all vertices as not visited
 	bool visited[V];
 	memset(visited, 0, sizeof(visited));
+
 	// Create a queue, enqueue source vertex and mark source vertex
 	// as visited
 	queue <int> q;
 	q.push(s);
 	visited[s] = true;
 	parent[s] = -1;
+
 	// Standard BFS Loop
+	// TODO: parallelise
 	while (!q.empty())
 	{
 		int u = q.front();
@@ -36,9 +42,11 @@ bool bfs(int rGraph[V][V], int s, int t, int parent[])
 			{
 				q.push(v);
 				parent[v] = u;
+
 				visited[v] = true;
 			}
 	}
+
 	// If we reached sink in BFS starting from source, then return
 	// true, else false
 	return visited[t] == true;
@@ -47,7 +55,9 @@ bool bfs(int rGraph[V][V], int s, int t, int parent[])
 // Returns tne maximum flow from s to t in the given graph
 int edmondsKarp(int graph[V][V], int s, int t)
 {
+	omp_set_num_threads(5);
 	int u, v;
+
 	// Create a residual graph and fill the residual graph with
 	// given capacities in the original graph as residual capacities
 	// in residual graph
@@ -57,28 +67,58 @@ int edmondsKarp(int graph[V][V], int s, int t)
 	for (u = 0; u < V; u++)
 		for (v = 0; v < V; v++)
 			rGraph[u][v] = graph[u][v];
+
 	int parent[V];  // This array is filled by BFS and to store path
 	int max_flow = 0;  // There is no flow initially
-	// Augment the flow while tere is path from source to sink
+
+	// Augment the flow while there is path from source to sink
 	while (bfs(rGraph, s, t, parent))
 	{
 		// Find minimum residual capacity of the edges along the
 		// path filled by BFS. Or we can say find the maximum flow
 		// through the path found.
+		// TODO: check parallel works here
 		int path_flow = INT_MAX;
-		for (v = t; v != s; v = parent[v])
-		{
+
+		// uh oh
+		int path[V];
+		int ii = 0;
+		for (v = t; v != s; v = parent[v]) {
+			path[ii] = v;
+			ii++;
+		}
+
+		//#pragma omp parallel for private(path_flow)
+		// for (v = t; v != s; v = parent[v]) {
+		// 	u = parent[v];
+		// 	path_flow = min(path_flow, rGraph[u][v]);
+		// }
+		#pragma omp parallel for reduction(min:path_flow)
+		for (int jj = ii-1; jj >= 0; jj--) {
+			v = path[jj];
 			u = parent[v];
 			path_flow = min(path_flow, rGraph[u][v]);
 		}
+
+
 		// update residual capacities of the edges and reverse edges
 		// along the path
-		for (v = t; v != s; v = parent[v])
-		{
+		// TODO: check parallel works here
+		// #pragma omp parallel for
+		// for (v = t; v != s; v = parent[v]) {
+		// 	u = parent[v];
+		// 	rGraph[u][v] -= path_flow;
+		// 	rGraph[v][u] += path_flow;
+		// }
+		#pragma omp parallel for
+		for (int jj = ii-1; jj >= 0; jj--) {
+			v = path[jj];
 			u = parent[v];
 			rGraph[u][v] -= path_flow;
 			rGraph[v][u] += path_flow;
 		}
+
+
 		// Add path flow to overall flow
 		max_flow += path_flow;
 	}
@@ -145,5 +185,5 @@ int main() {
 	// TODO: scale rational to int
 	int min = minimax(graph, n_nodes);
 	cout << min << endl;
-
+	return 0;
 }
